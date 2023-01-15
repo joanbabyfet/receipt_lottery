e7f157abb2def5cde594679e15eb81e607d735ca
alan <crwu0206@gmail.com>
2023-01-15 22:50:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## About
+基于 Python3 采集统一发票中奖号码并自动对奖
+
+## Feature
+
+* 采集发票中奖号码, 网站以 https://invoice.etax.nat.gov.tw/index.html 为例
+* 引用 BeautifulSoup 库, 实现选择html DOM
+* 实现输入号码并自动对奖功能
+
+## Requires
+Python 3.11.0  
+beautifulsoup4 4.11.1  
+requests 2.28.1  
+
+## Usage
+```
+python main.py
+```
+
+## Change Log
+v1.0.0  
+
+## Maintainers
+Alan
+
+## LICENSE
+[MIT License](https://github.com/joanbabyfet/receipt_lottery/blob/master/LICENSE)
---
+++ main.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    url = 'https://invoice.etax.nat.gov.tw/index.html' # 统一发票中奖号
+    resp = requests.get(url)
+    resp.encoding = 'utf-8' # 使用与网页相对应的编码格式, 避免乱码
+    soup = BeautifulSoup(resp.text, 'html.parser') # 通过html dom解析器采集数据
+
+    data = soup.select('.container-fluid .etw-tbiggest')
+    prize_s = data[0].get_text().strip() # 特别奖, 使用strip()避免出现换行符\n
+    prize_1 = data[1].get_text().strip() # 特奖
+    prize_2 = [data[2].get_text().strip(), data[3].get_text().strip(), data[4].get_text().strip()] # 头奖
+
+    while True: # 实现一直输入号码并对奖
+        try:
+            number = input('请输入发票号码:')
+
+            if number == prize_s: 
+                print('对中 1,000 萬元')
+            elif number == prize_1: 
+                print('对中 200 萬元')
+
+            for v in prize_2:
+                if number == v:
+                    print('对中 20 萬元')
+                    break
+                elif number[-7:] == v[-7:]: # 获取后面7位数字
+                    print('对中 4 萬元')
+                    break
+                elif number[-6:] == v[-6:]:
+                    print('对中 1 萬元')
+                    break
+                elif number[-5:] == v[-5:]:
+                    print('对中 4 千元')
+                    break
+                elif number[-4:] == v[-4:]:
+                    print('对中 1 千元')
+                    break
+                elif number[-3:] == v[-3:]:
+                    print('对中 2 百元')
+                    break
+        except:
+            break
+
+if __name__ == '__main__': # 主入口
+    main()
